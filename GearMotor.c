c6bf3b67c1226da90aeb5d1466771daf0c315c26
unknown <robotics@C703.cchs.org>
2015-02-21 15:08:43 -0500

    Initial revision from evanbaren's code

---
+++ GearMotor.c
@@ -0,0 +1,11 @@
+#pragma config(Hubs,  S1, HTMotor,  none,     none,     none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     NED,            sensorTouch)
+#pragma config(Motor,  mtr_S1_C1_1,     leftdrive,     tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C1_2,     rightdrive,    tmotorTetrix, openLoop)
+
+task main()
+{
+
+
+}
